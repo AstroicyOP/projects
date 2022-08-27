96157ae0ba914cfbba6758feec70d73592b6cead
Astroicy <109760180+AstroicyOP@users.noreply.github.com>
2022-08-27 23:07:12 +0200

    Create alestro.js

---
+++ bot/js/alestro.js
@@ -0,0 +1,13 @@
+# Alestro Discord bot
+
+# Bot info
+- DJS Version - v13
+- Updated - True
+- Beta - False
+- Nsfw - False
+- Sfw - True
+  
+# Repo
+- Open source - False
+- Security - true
+- Stop = True
