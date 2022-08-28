e19f39386ce12898d7ad264eaff73e6c590eaea5
Astroicy <109760180+AstroicyOP@users.noreply.github.com>
2022-08-28 12:31:24 +0200

    somecord discord api wrapper
    forked and maintained version of discatpy

---
+++ api/wrapper/somecord.py
@@ -0,0 +1,16 @@
+# Somecord
+
+# API INFO
+Discord = True,
+Discord_Bot = True,
+Language_discord_api = "Dasgo, Go", "discord.js API Types, JavaScript",
+Language_somecord = Python3
+Language_somecord_version = 3.8, 3.9, // Bumping into 3.9 
+
+# SOMECORD INFO
+api_wrapper_name = Somecord,
+forked = True,
+main_name = DisCatyPy,
+Python = True,
+
+# I LOVE YOU // HARSH IS GAY 
